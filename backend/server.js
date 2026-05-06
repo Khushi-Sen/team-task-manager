9ea41d08fec45da7b2adb8d3f4ade54996f63101
Khushi-Sen <khushi.sen@spsu.ac.in>
2026-05-06 12:14:09 +0530

    Initial full-stack task manager

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log('MongoDB connected'))
+.catch(err=>console.log(err));
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/projects', require('./routes/projects'));
+app.use('/api/tasks', require('./routes/tasks'));
+
+app.get('/', (req,res)=>res.send('API Running'));
+
+app.listen(process.env.PORT || 5000, ()=>{
+  console.log('Server running');
+});
